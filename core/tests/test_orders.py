4f1ad5ce3fc433c66f475a6a2ee43c92322ca744
suportebeloj <suportebeloj@gmail.com>
2022-05-19 15:50:22 -0300

    Initial tests of model "Order" passed.

--- core/tests/test_orders.py
+++ core/tests/test_orders.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from orders.models import Product
+from orders.models import Product, Order
 
 
 class ProductTestCase(TestCase):
@@ -27,3 +27,43 @@ class ProductTestCase(TestCase):
         self.assertEqual(float(prod2.unitary_value), 0)
         self.assertEqual(prod2.quantity_stock, 0)
 
+
+class OrderTestCase(TestCase):
+    def setUp(self) -> None:
+        prod = Product.objects.create(name="Smartphone X Mark 2022", unitary_value=2635.33, quantity_stock=8)
+        Order.objects.create(
+            product=prod,
+            quantity=12,
+            unitary_value=2023.39,
+            applicant="client 01",
+            zip_code=55567505,
+            uf="AZ",
+            city="city of abc",
+            district="zone 0002",
+            street="street of news",
+            number=100,
+            forwarding_agent="agent 001",
+        )
+
+    def test_model_values(self):
+        order = Order.objects.get(applicant="client 01")
+        self.assertEqual(order.product.id, 1)
+        self.assertEqual(order.quantity, 12)
+        self.assertEqual(float(order.unitary_value), 2023.39)
+        self.assertEqual(order.applicant, "client 01")
+        self.assertEqual(order.zip_code, 55567505)
+        self.assertEqual(order.uf, "AZ")
+        self.assertEqual(order.city, "city of abc")
+        self.assertEqual(order.district, "zone 0002")
+        self.assertEqual(order.street, "street of news")
+        self.assertEqual(order.number, 100)
+        self.assertEqual(order.forwarding_agent, "agent 001")
+        self.assertEqual(order.order_situation, "p")
+
+    def test_order_situation(self):
+        order = Order.objects.get(applicant="client 01")
+        self.assertEqual(order.order_situation, "p")
+        order.order_situation = 's'
+        self.assertEqual(order.order_situation, "s")
+        order.order_situation = 'd'
+        self.assertEqual(order.order_situation, "d")
