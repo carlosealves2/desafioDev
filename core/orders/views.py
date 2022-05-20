1b1f626006fa307c4ac3592327f3f8e0ad3ca28a
suportebeloj <suportebeloj@gmail.com>
2022-05-19 21:08:59 -0300

    initial views "index" and "products"

--- core/orders/views.py
+++ core/orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Order, Product
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def products(request):
+    return render(request, 'products.html', {
+        'product_list': Product.objects.all()
+    })
